05a9ddd71f3260285a99783e995303ea0e473e3b
Deron <dbsmith@uga.edu>
2017-04-05 07:16:04 -0400

    Updates to url_list.txt file read-in

--- HMSDaymet/HMSDaymet.cs
+++ HMSDaymet/HMSDaymet.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Web;
 using System.Threading.Tasks;
 
 namespace HMSDaymet
@@ -59,20 +60,24 @@ namespace HMSDaymet
         private string GetDaymetURL(out string errorMsg, string datasource, string dataSet)
         {
             errorMsg = "";
-            string prepInfo = System.AppDomain.CurrentDomain.BaseDirectory + @"bin\url_info.txt";  // URL configuration info.
+            //string prepInfo = System.AppDomain.CurrentDomain.BaseDirectory + @"bin\url_info.txt";  // URL configuration info.
             string urlStr = "";
             string[] lineData;
             try
             {
-                foreach( string line in File.ReadLines(prepInfo))
-                {
-                    lineData = line.Split(' ');
-                    if (lineData[0].Equals(datasource +"_" + dataSet + "_URL", StringComparison.OrdinalIgnoreCase))
-                    {
-                        urlStr = lineData[1];
-                        break;
-                    }
-                }
+
+				// Reading value from Application variables
+				Dictionary<string, string> urls = (Dictionary<string, string>)HttpContext.Current.Application["urlList"];
+				urlStr = urls[datasource + "_" + dataSet + "_URL"];
+                //foreach( string line in File.ReadLines(prepInfo))
+                //{
+                //    lineData = line.Split(' ');
+                //    if (lineData[0].Equals(datasource +"_" + dataSet + "_URL", StringComparison.OrdinalIgnoreCase))
+                //    {
+                //        urlStr = lineData[1];
+                //        break;
+                //    }
+                //}
             }
             catch
             {
