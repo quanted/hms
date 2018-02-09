c87374dd512f406626a2699908010126d7a6f620
dbsmith88 <dbsmith@uga.edu>
2018-02-09 11:25:14 -0600

    Added additional unit tests

--- Data.Source.Tests/NLDASTests.cs
+++ Data.Source.Tests/NLDASTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using Data.Source;
 
 namespace Data.Source.Tests
 {
@@ -40,18 +41,12 @@ namespace Data.Source.Tests
             timeSeries.Input = iFactory.SetTimeSeriesInput(validInput, new List<string>() { "PRECIP" }, out errorMsg);
         }
 
-        // NLDAS public Methods to be tested
+        // NLDAS remaining public Methods to be implemented
 
-        // GetData(out string errorMsg, string dataset, ITimeSeriesInput componentInput)
-        // AdjustForOffset(out string errorMsg, ITimeSeriesInput cInput)
-        // SetDateToLocal(double offset, string dateHour, string dateFormat)
         // ConstructURL(out string errorMsg, string dataset, ITimeSeriesInput cInput)
         // DetermineReturnCoordinates(out string errorMsg, IPointCoordinate point)
         // SetDataToOutput(out string errorMsg, string dataset, string data, ITimeSeriesOutput output, ITimeSeriesInput input)
         // CheckStatus(string dataset, ITimeSeriesInput testInput)
-
-
-
 
 
         [TestMethod]
@@ -79,19 +74,133 @@ namespace Data.Source.Tests
             Assert.AreEqual("ERROR: Unable to download requested nldas data. Invalid URI: The format of the URI could not be determined.", errorMsg);
         }
 
-
+        /// <summary>
+        /// Valid test for AdjustForOffset, base
+        /// </summary>
         [TestMethod]
-        public void AdjustForOffsetValid()
+        public void AdjustForOffsetValidTest1()
         {
             BuildITimeSeries();
             NLDAS nldas = new NLDAS();
             string errorMsg = "";
             timeSeries.Input.TimeLocalized = true;
-            string dataset = "Precipitation";
             timeSeries.Input.DateTimeSpan = NLDAS.AdjustForOffset(out errorMsg, timeSeries.Input) as DateTimeSpan;
             Assert.AreEqual("", errorMsg);
         }
 
-        //TODO: AdjustForOffsetUnitTests, 1. Valid test, 2. Valid test for Offset -7 and timelocalized=true, 3. Valid test for Offset +7 and timelocalized=true, 4. Valid test with timelocalized=false, 5. Invalid timezone range
+        /// <summary>
+        /// Valid test for AdjustForOffset, TimeLocalized = true and Offset = -7
+        /// </summary>
+        [TestMethod]
+        public void AdjustForOffsetValidTest2()
+        {
+            BuildITimeSeries();
+            NLDAS nldas = new NLDAS();
+            string errorMsg = "";
+            timeSeries.Input.TimeLocalized = true;
+            timeSeries.Input.Geometry.Timezone.Offset = -7;
+            timeSeries.Input.DateTimeSpan = NLDAS.AdjustForOffset(out errorMsg, timeSeries.Input) as DateTimeSpan;
+            string startDate = timeSeries.Input.DateTimeSpan.StartDate.ToString();
+            string endDate = timeSeries.Input.DateTimeSpan.EndDate.ToString();
+            Assert.AreEqual("1/1/2015 8:00:00 AM", startDate);
+            Assert.AreEqual("1/9/2015 7:00:00 AM", endDate);
+        }
+
+        /// <summary>
+        /// Valid test for AdjustForOffset, TimeLocalized = true and Offset = 7
+        /// </summary>
+        [TestMethod]
+        public void AdjustForOffsetValidTest3()
+        {
+            BuildITimeSeries();
+            NLDAS nldas = new NLDAS();
+            string errorMsg = "";
+            timeSeries.Input.TimeLocalized = true;
+            timeSeries.Input.Geometry.Timezone.Offset = 7;
+            timeSeries.Input.DateTimeSpan = NLDAS.AdjustForOffset(out errorMsg, timeSeries.Input) as DateTimeSpan;
+            string startDate = timeSeries.Input.DateTimeSpan.StartDate.ToString();
+            string endDate = timeSeries.Input.DateTimeSpan.EndDate.ToString();
+            Assert.AreEqual("12/31/2014 5:00:00 PM", startDate);
+            Assert.AreEqual("1/8/2015 5:00:00 PM", endDate);
+        }
+
+        /// <summary>
+        /// Valid test for AdjustForOffset, TimeLocalized = false
+        /// </summary>
+        [TestMethod]
+        public void AdjustForOffsetValidTest4()
+        {
+            BuildITimeSeries();
+            NLDAS nldas = new NLDAS();
+            string errorMsg = "";
+            timeSeries.Input.TimeLocalized = false;
+            timeSeries.Input.Geometry.Timezone.Offset = 7;
+            timeSeries.Input.DateTimeSpan = NLDAS.AdjustForOffset(out errorMsg, timeSeries.Input) as DateTimeSpan;
+            string startDate = timeSeries.Input.DateTimeSpan.StartDate.ToString();
+            string endDate = timeSeries.Input.DateTimeSpan.EndDate.ToString();
+            Assert.AreEqual("1/1/2015 1:00:00 AM", startDate);
+            Assert.AreEqual("1/9/2015 12:00:00 AM", endDate);
+        }
+
+        /// <summary>
+        /// Invalid test for AdjustForOffset, TimeLocalized = true, Offset = 24
+        /// </summary>
+        [TestMethod]
+        public void AdjustForOffsetInvalidOffsetTest()
+        {
+            BuildITimeSeries();
+            NLDAS nldas = new NLDAS();
+            string errorMsg = "";
+            timeSeries.Input.TimeLocalized = true;
+            timeSeries.Input.Geometry.Timezone.Offset = 24;
+            timeSeries.Input.DateTimeSpan = NLDAS.AdjustForOffset(out errorMsg, timeSeries.Input) as DateTimeSpan;
+            string startDate = timeSeries.Input.DateTimeSpan.StartDate.ToString();
+            string endDate = timeSeries.Input.DateTimeSpan.EndDate.ToString();
+            Assert.AreEqual("12/31/2014 12:00:00 AM", startDate);
+        }
+
+        /// <summary>
+        /// Valid test for SetDateToLocal, offset = -5
+        /// </summary>
+        [TestMethod]
+        public void SetDateToLocalValidTest1()
+        {
+            BuildITimeSeries();
+            string startDate = NLDAS.SetDateToLocal(
+                -5,
+                this.timeSeries.Input.DateTimeSpan.StartDate.ToString("yyyy-MM-dd HH"),
+                this.timeSeries.Input.DateTimeSpan.DateTimeFormat);
+            string expectedStartDate = "2014-12-31 19";
+            Assert.AreEqual(expectedStartDate, startDate);
+        }
+
+        /// <summary>
+        /// Valid test for SetDateToLocal, offset = 5
+        /// </summary>
+        [TestMethod]
+        public void SetDateToLocalValidTest2()
+        {
+            BuildITimeSeries();
+            string startDate = NLDAS.SetDateToLocal(
+                5,
+                this.timeSeries.Input.DateTimeSpan.StartDate.ToString("yyyy-MM-dd HH"),
+                this.timeSeries.Input.DateTimeSpan.DateTimeFormat);
+            string expectedStartDate = "2015-01-01 05";
+            Assert.AreEqual(expectedStartDate, startDate);
+        }
+
+        /// <summary>
+        /// Invalid test for SetDateToLocal, invalid DateTimeFormat
+        /// </summary>
+        [TestMethod]
+        public void SetDateToLocalInvalidTest()
+        {
+            BuildITimeSeries();
+            string startDate = NLDAS.SetDateToLocal(
+                -5,
+                this.timeSeries.Input.DateTimeSpan.StartDate.ToString("yyyy-MM-dd HH"),
+                "yyyyyyy");
+            Assert.AreEqual("0002014", startDate);
+        }
     }
 }
