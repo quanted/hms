396204d6185b17ac23a69605b3f13d595feb9251
JKob0233 <koblich.joshua@epa.gov>
2018-06-06 15:35:06 -0400

    Fixes to integration tests.

--- Web.Services.Tests/EvapotranspirationController.IntegrationTests.cs
+++ Web.Services.Tests/EvapotranspirationController.IntegrationTests.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using System.Text;
 using Data;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Services.Tests
 {
@@ -49,7 +50,7 @@ namespace Web.Services.Tests
         /// <summary>
         /// Priestly-Taylor NLDAS request json string for testing a valid request
         /// </summary>
-        const string priestlytaylorNldasRequest = "{ \"source\": \"nldas\", \"algorithm\": \"priestlytaylor\",  \"Albedo\": 0.23, \"dateTimeSpan\": { \"startDate\": \"2009-12-31T00:00:00\", " +
+        const string priestlytaylorNldasRequest = "{ \"source\": \"nldas\", \"algorithm\": \"priestlytaylor\", \"Albedo\": 0.23, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
             "\"endDate\": \"2009-12-31T00:00:00\", \"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
             "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
@@ -58,8 +59,9 @@ namespace Web.Services.Tests
         /// <summary>
         /// Granger-Gray NLDAS request json string for testing a valid request
         /// </summary>
-        const string grangergrayNldasRequest = "{ \"source\": \"nldas\", \"algorithm\": \"grangergray\", \"Albedo\": 0.23, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
-            "\"endDate\": \"2009-12-31T00:00:00\", \"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
+        const string grangergrayNldasRequest =
+            "{\"source\": \"nldas\", \"algorithm\": \"grangergray\", \"Albedo\": 0.23, \"dateTimeSpan\": {\"startDate\": \"2009-01-01T00:00:00\",\"endDate\": \"2009-12-31T00:00:00\"," +
+            "\"dateTimeFormat\": \"yyyy-MM-dd HH\"},\"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
             "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
             "\"units\": \"default\",\"outputFormat\": \"json\"}";
@@ -77,8 +79,8 @@ namespace Web.Services.Tests
         /// McJannett NLDAS request json string for testing a valid request
         /// </summary>
         const string mcjannettNldasRequest = "{ \"source\": \"nldas\", \"algorithm\": \"mcjannett\", \"Albedo\": 0.23, \"Lake Surface Area\": 0.005, " +
-            "\"Average Lake Depth\": 0.2, \"JanuaryTemp\": 1.0, \"FebruaryTemp\": 1.0, \"MarchTemp\": 1.0, \"AprilTemp\": 1.0, \"MayTemp\": 1.0, \"JuneTemp\": 1.0, \"JulyTemp\": 1.0, " +
-            "\"AugustTemp\": 1.0, \"SeptemberTemp\": 1.0, \"OctoberTemp\": 1.0, \"NovemberTemp\": 1.0, \"DecemberTemp\": 1.0, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", \"endDate\": \"2009-12-31T00:00:00\", " +
+            "\"Average Lake Depth\": 0.2, \"AirTemperature\": {\"1\": 1.0, \"2\": 1.0, \"3\": 1.0, \"4\": 1.0, \"5\": 1.0, \"6\": 1.0, \"7\": 1.0, " +
+            "\"8\": 1.0, \"9\": 1.0, \"10\": 1.0, \"11\": 1.0, \"12\": 1.0}, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", \"endDate\": \"2009-12-31T00:00:00\", " +
             "\"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
             "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
@@ -133,9 +135,8 @@ namespace Web.Services.Tests
         /// Shuttleworth-Wallace NLDAS request json string for testing a valid request
         /// </summary>
         const string shuttleworthwallaceNldasRequest = "{ \"source\": \"nldas\", \"algorithm\": \"shuttleworthwallace\", \"Albedo\": 0.23, \"Subsurface Resistance\": 500.0, " +
-            "\"Stomatal Resistance\": 400.0, \"Leaf Width\": 0.02, \"Roughness Length\": 0.02, \"Vegetation Height\": 0.12, \"JanuaryIndex\": 2.51, \"FebruaryIndex\": 2.51, \"MarchIndex\": 2.51, " +
-            "\"AprilIndex\": 2.51, \"MayIndex\": 2.51, \"JuneIndex\": 2.51, \"JulyIndex\": 2.51, \"AugustIndex\": 2.51, \"SeptemberIndex\": 2.51, \"OctoberIndex\": 2.51, \"NovemberIndex\": 2.51, " +
-            "\"DecemberIndex\": 2.51, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
+            "\"Stomatal Resistance\": 400.0, \"Leaf Width\": 0.02, \"Roughness Length\": 0.02, \"Vegetation Height\": 0.12, \"LeafAreaIndices\": {\"1\": 1.0, \"2\": 1.0, \"3\": 1.0, \"4\": 1.0, \"5\": 1.0, \"6\": 1.0, \"7\": 1.0, " +
+            "\"8\": 1.0, \"9\": 1.0, \"10\": 1.0, \"11\": 1.0, \"12\": 1.0}, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
             "\"endDate\": \"2009-12-31T00:00:00\", \"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
             "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
@@ -169,7 +170,7 @@ namespace Web.Services.Tests
         /// </summary>
         const string priestlytaylorGldasRequest = "{ \"source\": \"gldas\", \"algorithm\": \"priestlytaylor\",  \"Albedo\": 0.23, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
             "\"endDate\": \"2009-12-31T00:00:00\", \"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
-            "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
+            "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\", \"Albedo\": 0.23}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
             "\"units\": \"default\",\"outputFormat\": \"json\"}";
 
@@ -178,7 +179,7 @@ namespace Web.Services.Tests
         /// </summary>
         const string grangergrayGldasRequest = "{ \"source\": \"gldas\", \"algorithm\": \"grangergray\", \"Albedo\": 0.23, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
             "\"endDate\": \"2009-12-31T00:00:00\", \"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
-            "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
+            "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\", \"Albedo\": 0.23}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
             "\"units\": \"default\",\"outputFormat\": \"json\"}";
 
@@ -194,9 +195,9 @@ namespace Web.Services.Tests
         /// <summary>
         /// McJannett gldas request json string for testing a valid request
         /// </summary>
-        const string mcjannettGldasRequest = "{ \"source\": \"gldas\", \"algorithm\": \"mcjannett\", \"Albedo\": 0.23, \"Lake Surface Area\": 0.005, " +
-            "\"Average Lake Depth\": 0.2, \"JanuaryTemp\": 1.0, \"FebruaryTemp\": 1.0, \"MarchTemp\": 1.0, \"AprilTemp\": 1.0, \"MayTemp\": 1.0, \"JuneTemp\": 1.0, \"JulyTemp\": 1.0, " +
-            "\"AugustTemp\": 1.0, \"SeptemberTemp\": 1.0, \"OctoberTemp\": 1.0, \"NovemberTemp\": 1.0, \"DecemberTemp\": 1.0, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", \"endDate\": \"2009-12-31T00:00:00\", " +
+        const string mcjannettGldasRequest = "{ \"source\": \"nldas\", \"algorithm\": \"mcjannett\", \"Albedo\": 0.23, \"Lake Surface Area\": 0.005, " +
+            "\"Average Lake Depth\": 0.2, \"AirTemperature\": {\"1\": 1.0, \"2\": 1.0, \"3\": 1.0, \"4\": 1.0, \"5\": 1.0, \"6\": 1.0, \"7\": 1.0, " +
+            "\"8\": 1.0, \"9\": 1.0, \"10\": 1.0, \"11\": 1.0, \"12\": 1.0}, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", \"endDate\": \"2009-12-31T00:00:00\", " +
             "\"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
             "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
@@ -251,9 +252,8 @@ namespace Web.Services.Tests
         /// Shuttleworth-Wallace gldas request json string for testing a valid request
         /// </summary>
         const string shuttleworthwallaceGldasRequest = "{ \"source\": \"gldas\", \"algorithm\": \"shuttleworthwallace\", \"Albedo\": 0.23, \"Subsurface Resistance\": 500.0, " +
-            "\"Stomatal Resistance\": 400.0, \"Leaf Width\": 0.02, \"Roughness Length\": 0.02, \"Vegetation Height\": 0.12, \"JanuaryIndex\": 2.51, \"FebruaryIndex\": 2.51, \"MarchIndex\": 2.51, " +
-            "\"AprilIndex\": 2.51, \"MayIndex\": 2.51, \"JuneIndex\": 2.51, \"JulyIndex\": 2.51, \"AugustIndex\": 2.51, \"SeptemberIndex\": 2.51, \"OctoberIndex\": 2.51, \"NovemberIndex\": 2.51, " +
-            "\"DecemberIndex\": 2.51, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
+            "\"Stomatal Resistance\": 400.0, \"Leaf Width\": 0.02, \"Roughness Length\": 0.02, \"Vegetation Height\": 0.12, \"LeafAreaIndices\": {\"1\": 1.0, \"2\": 1.0, \"3\": 1.0, \"4\": 1.0, \"5\": 1.0, \"6\": 1.0, \"7\": 1.0, " +
+            "\"8\": 1.0, \"9\": 1.0, \"10\": 1.0, \"11\": 1.0, \"12\": 1.0}, \"dateTimeSpan\": { \"startDate\": \"2009-01-01T00:00:00\", " +
             "\"endDate\": \"2009-12-31T00:00:00\", \"dateTimeFormat\": \"yyyy-MM-dd HH\" }, \"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
             "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
@@ -314,7 +314,7 @@ namespace Web.Services.Tests
         const string ncdcRequest =
             "{\"source\": \"ncdc\", \"algorithm\": \"hamon\", \"dateTimeSpan\": {\"startDate\": \"2009-01-01T00:00:00\",\"endDate\": \"2009-12-31T00:00:00\"," +
             "\"dateTimeFormat\": \"yyyy-MM-dd HH\"},\"geometry\": {\"description\": \"EPA Athens Office\",\"point\": " +
-            "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
+            "{\"latitude\": 33.925673,\"longitude\": -83.355723},\"geometryMetadata\": {\"stationID\": \"GHCND:USW00013874\",\"City\": \"Athens\",\"State\": \"Georgia\",\"Country\": \"United States\"}," +
             "\"timezone\": {\"name\": \"EST\",\"offset\": -5,\"dls\": false}},\"dataValueFormat\": \"E3\",\"temporalResolution\": \"daily\",\"timeLocalized\": true," +
             "\"units\": \"default\",\"outputFormat\": \"json\"}";
 
@@ -325,6 +325,7 @@ namespace Web.Services.Tests
         {
             _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             _client = _server.CreateClient();
+            _client.Timeout = new System.TimeSpan(0, 4, 0); //Many tests were failing due to timeout errors
         }
 
         /// <summary>
@@ -334,44 +335,46 @@ namespace Web.Services.Tests
         /// <returns></returns>
         [Trait("Priority", "1")]
         [Theory]
-        [InlineData(nldasRequest)]
-        [InlineData(gldasRequest)]
-        [InlineData(hamonNldasRequest)]
-        [InlineData(priestlytaylorNldasRequest)]
-        [InlineData(grangergrayNldasRequest)]
-        [InlineData(penpanNldasRequest)]
-        [InlineData(mcjannettNldasRequest)]
-        [InlineData(penmanopenwaterNldasRequest)]
-        [InlineData(penmandailyNldasRequest)]
-        [InlineData(penmanhourlyNldasRequest)]
-        [InlineData(mortoncraeNldasRequest)]
-        [InlineData(mortoncrweNldasRequest)]
-        [InlineData(shuttleworthwallaceNldasRequest)]
-        [InlineData(hspfNldasRequest)]
-        [InlineData(hamonGldasRequest)]
-        [InlineData(priestlytaylorGldasRequest)]
-        [InlineData(grangergrayGldasRequest)]
+        [InlineData(nldasRequest)]                      
+        [InlineData(gldasRequest)]                      
+        [InlineData(hamonNldasRequest)]                 
+        [InlineData(priestlytaylorNldasRequest)]        
+        [InlineData(grangergrayNldasRequest)]         
+        [InlineData(penpanNldasRequest)]              
+        [InlineData(mcjannettNldasRequest)]           
+        [InlineData(penmanopenwaterNldasRequest)]     
+        [InlineData(penmandailyNldasRequest)]         
+        [InlineData(penmanhourlyNldasRequest)]        
+        [InlineData(mortoncraeNldasRequest)]            
+        [InlineData(mortoncrweNldasRequest)]            
+        [InlineData(shuttleworthwallaceNldasRequest)] 
+        [InlineData(hspfNldasRequest)]                
+        [InlineData(hamonGldasRequest)]                
+        [InlineData(priestlytaylorGldasRequest)]        
+        [InlineData(grangergrayGldasRequest)]         
         [InlineData(penpanGldasRequest)]
         [InlineData(mcjannettGldasRequest)]
-        [InlineData(penmanopenwaterGldasRequest)]
-        [InlineData(penmandailyGldasRequest)]
-        [InlineData(penmanhourlyGldasRequest)]
-        [InlineData(mortoncraeGldasRequest)]
-        [InlineData(mortoncrweGldasRequest)]
-        [InlineData(shuttleworthwallaceGldasRequest)]
-        [InlineData(hspfGldasRequest)]
-        [InlineData(hamonDaymetRequest)]
-        [InlineData(priestlytaylorDaymetRequest)]
-        [InlineData(mortoncraeDaymetRequest)]
-        [InlineData(mortoncrweDaymetRequest)]
+        [InlineData(penmanopenwaterGldasRequest)]     
+        [InlineData(penmandailyGldasRequest)]         
+        [InlineData(penmanhourlyGldasRequest)]        
+        [InlineData(mortoncraeGldasRequest)]          
+        [InlineData(mortoncrweGldasRequest)]          
+        [InlineData(shuttleworthwallaceGldasRequest)] 
+        [InlineData(hspfGldasRequest)]                
+        [InlineData(hamonDaymetRequest)]                
+        [InlineData(priestlytaylorDaymetRequest)]       
+        [InlineData(mortoncraeDaymetRequest)]           
+        [InlineData(mortoncrweDaymetRequest)]           
+        [InlineData(ncdcRequest)]                      
         public async Task ValidRequests(string evapoInputString)
         {
             string endpoint = "api/hydrology/evapotranspiration";
             EvapotranspirationInput input = JsonConvert.DeserializeObject<EvapotranspirationInput>(evapoInputString);
             Debug.WriteLine("Integration Test: Evapotranspiration controller; Endpoint: " + endpoint + "; Data source: " + input.Source);
+            string json = JsonConvert.SerializeObject(input);
             var response = await _client.PostAsync(
                 endpoint,
-                new StringContent(JsonConvert.SerializeObject(input), Encoding.UTF8, "application/json"));
+                new StringContent(json, Encoding.UTF8, "application/json"));
 
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadAsStringAsync();
