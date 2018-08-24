cbe389ca590080d4b5e4c44ab65244a7ebd4194e
dbsmith88 <dbsmith@uga.edu>
2018-08-24 07:39:55 -0500

    added sqlite class to utilities and added in query calls to curvenumber for centroid and cn dictionary

---
+++ Utilities/SQLite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+
+namespace Utilities
+{
+    public class SQLite
+    {
+
+        /// <summary>
+        /// Access sqlite database file, dbPath, and executes the query provided returning the query results
+        /// </summary>
+        /// <param name="dbPath">Path to the sqlite database</param>
+        /// <param name="query">Query to be executed on sqlite database</param>
+        /// <returns></returns>
+        public static Dictionary<string, string> GetData(string dbPath, string query)
+        {
+            if (query.Substring(0,6).ToUpper() != "SELECT")
+            {
+                return null;
+            }
+            // TODO: Dictionary object here is not sufficient for complete data retrieval from database.
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            using (System.Data.SQLite.SQLiteConnection con = new System.Data.SQLite.SQLiteConnection(dbPath))
+            {
+                using (System.Data.SQLite.SQLiteCommand com = new System.Data.SQLite.SQLiteCommand(con))
+                {
+                    con.Open();                             
+                    com.CommandText = query;                
+                    com.ExecuteNonQuery();                
+                    using (System.Data.SQLite.SQLiteDataReader reader = com.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            data.Add(reader["key"].ToString(), reader["value"].ToString());
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            return data;
+        }
+        
+    }
+}
