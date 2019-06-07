870e0b381b2a0ea520429492d10f0c6ce546cbbd
Koblich <koblich.joshua@epa.gov>
2019-06-07 16:33:27 -0400

    Fixed error

--- Web.Services/Models/WSEvapotranspiration.cs
+++ Web.Services/Models/WSEvapotranspiration.cs
@@ -72,42 +72,5 @@ namespace Web.Services.Models
 
             return result;
         }
-
-        /// <summary>
-        /// Gets Hamon evapotranspiration data using the given TimeSeriesInput parameters.
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public async Task<ITimeSeriesOutput> GetHamonEvapotranspiration(EvapotranspirationInput input)
-        {
-            string errorMsg = "";
-
-            // Constructs default error output object containing error message.
-            Utilities.ErrorOutput err = new Utilities.ErrorOutput();
-
-            // Validate evapotranspiration sources.
-            errorMsg = (!Enum.TryParse(input.Source, true, out EvapoSources pSource)) ? "ERROR: 'Source' was not found or is invalid." : "";
-            if (errorMsg.Contains("ERROR")) { return err.ReturnError(errorMsg); }
-
-            // Evapotranspiration object
-            Evapotranspiration.Evapotranspiration evapo = new Evapotranspiration.Evapotranspiration();
-
-            // ITimeSeriesInputFactory object used to validate and initialize all variables of the input object.
-            ITimeSeriesInputFactory iFactory = new TimeSeriesInputFactory();
-            evapo.Input = iFactory.SetTimeSeriesInput(input, new List<string>() { "evapotranspiration" }, out errorMsg);
-
-            // If error occurs in input validation and setup, errorMsg is added to metadata of an empty object.
-            if (errorMsg.Contains("ERROR")) { return err.ReturnError(errorMsg); }
-
-            // Gets the Evapotranspiration data.
-            ITimeSeriesOutput result = evapo.GetHamonData(out errorMsg);
-            if (errorMsg.Contains("ERROR")) { return err.ReturnError(errorMsg); }
-
-            // Get generic statistics
-            // TODO: Handle negative values
-            // result = Utilities.Statistics.GetStatistics(out errorMsg, evapo.Input, result);
-
-            return result;
-        }
     }
 }
