cabdf6c7f433bd53b5a785ba677dd374e9ee6392
jsclough <jclough@warrenpinnacle.com>
2019-11-11 10:47:40 -0500

    GUI.AQUATOX.Electron test case added.

---
+++ GUI.AQUATOX.Electron/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ElectronNET.API;
+
+namespace GUI.AQUATOX.ElectronApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseElectron(args);
+    }
+}
