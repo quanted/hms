77420e049e1e479cbfb3e2e78288349420775b9e
Deron Smith <deron.brock@gmail.com>
2021-03-10 14:37:57 -0600

    bug fix for the order of a network

--- Web.Services/Models/WSStream.cs
+++ Web.Services/Models/WSStream.cs
@@ -42,42 +42,65 @@ namespace Web.Services.Models
 
         public List<List<int>> generateOrder(List<List<object>> networkTable)
         {
-            List<List<int>> networkOrder = new List<List<int>>();
-            List<List<int>> comidOrder = new List<List<int>>();
-            // Initialize network order table
-            for(int i = 0; i < networkTable.Count; i++)
+
+            List<List<object>> seq = new List<List<object>>();
+            List<int> dag = new List<int>();
+            for(int i = networkTable.Count - 1; i > 0; i--)
             {
-                networkOrder.Add(new List<int>());
+                int hydroseq = Int32.Parse(networkTable[i][1].ToString());
+                seq.Add(new List<object>());
+                seq[0].Add(networkTable[i]);
+                dag.Add(hydroseq);
+            }
+
+
+            List<List<int>> seqOrder = new List<List<int>>();
+            seqOrder.Add(new List<int>()
+            {
+                Int32.Parse(networkTable[1][1].ToString())
+            });
+            List<List<int>> comidOrder = new List<List<int>>();
+            comidOrder.Add(new List<int>()
+            {
+                Int32.Parse(networkTable[1][0].ToString())
+            });
+            for (int i = 2; i <= dag.Count; i++)
+            {
+                seqOrder.Add(new List<int>());
                 comidOrder.Add(new List<int>());
             }
-            // Build segment order
-            for(int i = networkTable.Count-1; i > 0; i--)
+            for (int i = 2; i <= dag.Count; i++)
             {
                 int comid = Int32.Parse(networkTable[i][0].ToString());
                 int hydroseq = Int32.Parse(networkTable[i][1].ToString());
+                int dnhydroseq = Int32.Parse(networkTable[i][3].ToString());
                 int uphydroseq = Int32.Parse(networkTable[i][2].ToString());
-
-                int orderLvl = 0;
-
-                for (int j = networkTable.Count-1; j >= 0; j--)
+                int seq_j = 0;
+                for (int j = seqOrder.Count - 1; j >= 0; j--)
                 {
-                    if (networkOrder[j].Contains(uphydroseq))
+                    if (seqOrder[j].Contains(dnhydroseq))
                     {
-                        orderLvl = j+1;
+                        seq_j = j + 1;
+                        break;
+                    }
+                    else if (seqOrder[j].Contains(uphydroseq))
+                    {
+                        seq_j = j - 1;
                         break;
                     }
                 }
-                networkOrder[orderLvl].Add(hydroseq);
-                comidOrder[orderLvl].Add(comid);
+                comidOrder[seq_j].Add(comid);
+                seqOrder[seq_j].Add(hydroseq);
             }
-            // List cleanup
-            for(int i = networkOrder.Count-1; i > 0; i--)
+
+            for (int i = dag.Count - 1; i >= 0; i--)
             {
-                if(comidOrder[i].Count == 0)
+                if (comidOrder[i].Count == 0)
                 {
                     comidOrder.RemoveAt(i);
                 }
             }
+            comidOrder.Reverse();
 
             return comidOrder;
         }
